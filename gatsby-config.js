a6049dada1655515c1f6e8996202e276dcbd1bbe
j-Riv <jose.alfredo.rivera@gmail.com>
2022-01-14 23:01:18 -0800

    init

--- gatsby-config.js
+++ gatsby-config.js
@@ -20,17 +20,17 @@ module.exports = {
     `gatsby-plugin-image`,
     `gatsby-plugin-sharp`,
     `gatsby-transformer-sharp`,
-    // {
-    //   resolve: `gatsby-plugin-google-gtag`,
-    //   options: {
-    //     // You can add multiple tracking ids and a pageview event will be fired for all of them.
-    //     trackingIds: [
-    //       process.env.G_TAG, // Google Analytics / GA
-    //       // "AW-CONVERSION_ID", // Google Ads / Adwords / AW
-    //       // "DC-FLOODIGHT_ID", // Marketing Platform advertising products (Display & Video 360, Search Ads 360, and Campaign Manager)
-    //     ],
-    //   },
-    // },
+    {
+      resolve: `gatsby-plugin-google-gtag`,
+      options: {
+        // You can add multiple tracking ids and a pageview event will be fired for all of them.
+        trackingIds: [
+          process.env.G_TAG, // Google Analytics / GA
+          // "AW-CONVERSION_ID", // Google Ads / Adwords / AW
+          // "DC-FLOODIGHT_ID", // Marketing Platform advertising products (Display & Video 360, Search Ads 360, and Campaign Manager)
+        ],
+      },
+    },
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
